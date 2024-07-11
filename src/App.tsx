570ccadeb6cd94113ba8517742b59b0bbc34b4e8
Esteban Vasco <evasco.27@hotmail.com>
2024-07-11 08:59:44 -0500

    Agregando estilos
    Se agrega los componentes scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import Header from './Header';
-import './App.css';
 
 function App() {
   return (
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,7 @@
 //Class Header
 
 import React from 'react';
+import './Header.scss';
 
 
 export default function Header() {
@@ -12,9 +13,10 @@ export default function Header() {
             <img src="./logo192.png" alt="logo" className='nav__img' />
           </div>
         
-            <div>
+            <div className='nav__links'>
                 <a href="#" className='nav__link nav__link--active'>Active</a>
                 <a href="#" className='nav__link'>disable</a>
+                <a href="#" className='nav__link'>Ver más</a>
             </div>
         </nav>
 
