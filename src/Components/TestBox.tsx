976d441f3fbb1a8b91633e602b8bcf9c84d3f835
Esteban Vasco <evasco.27@hotmail.com>
2024-07-11 11:36:38 -0500

    Pruebas cookies
    Pruebas para aceptar las cookies

--- src/Components/TestBox.tsx
+++ src/Components/TestBox.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useState } from "react";
 
 const TextBox = () => {
+
+    const [bgColor, setbgColor] = useState('slateblue');
+
     return (
         <div>
             <article title="Parrafo principal" 
@@ -8,6 +12,9 @@ const TextBox = () => {
                 {backgroundColor: 'slateblue'}
             }
             >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate eius aspernatur aliquid vero modi. Aliquam quisquam deserunt quidem ratione ex facilis. Et est hic, deserunt eligendi adipisci asperiores quisquam placeat.</article>
+            <button
+                onClick={() => setbgColor (bgColor === 'slateblue' ? ' indigo ' : 'slateblue') }
+            >Pulsa para cambiar</button>
         </div>
     );
 };
--- src/Test/TestBox.test.tsx
+++ src/Test/TestBox.test.tsx
@@ -1,18 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import TestBox from '../Components/TestBox';
 
 describe('TestBox component', () => {
+
+    let article;
+    let button;
+
+    beforeEach(() => {
+        render( < TestBox /> );
+        article = screen.getByRole('article', { name: 'Parrafo principal' });
+        button = screen.getByRole('button', { name: 'Pulsa para cambiar' });
+    })
     
     test('La caja de texto se encuentra en el documento', () => {
-       render( < TestBox /> );
-       const article = screen.getByRole('article', { name: 'Parrafo principal' });
         expect(article).toBeInTheDocument();
     });
 
     test('la caja con el texto tiene un color inicial', () => {
-        render( < TestBox /> );
-        const article = screen.getByRole('article', { name: 'Parrafo principal' });
         expect(article).toHaveStyle({backgroundColor: 'slateblue'});
     })
 
+    test('Comprobar si pulsar el boton cambia de fondo', () => {
+        fireEvent.click(button);
+        expect(article).toHaveStyle({
+            backgroundColor: 'indigo'
+        });
+    }
+    )
 })
